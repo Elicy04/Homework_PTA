6bad0614a81dc1fbfece75fb7c0ca6c0d127c154
Elicy04 <Elicy04@outlook.com>
2025-04-17 14:21:17 +0800

    1.1e_main完整提交.1

--- Algorithm/1.1e/1.1mTwoBigPoint.cpp
+++ Algorithm/1.1e/1.1mTwoBigPoint.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <algorithm>
 #include <utility>
+#include <stack>
+#include <limits>
 
 using namespace std;
 
+//对pair<int,int>重命名
+typedef pair<int,int> coord;
+
 // 对坐标基于横坐标的比较函数
-bool cmp(pair<int, int> x, pair<int, int> y)
+bool cmp(coord x, coord y)
 {
     return x.first > y.first;
 }
@@ -15,8 +20,9 @@ const int MAXN = 1002;
 //总坐标个数
 int n;
 //坐标数组
-pair<int,int> points[MAXN];
-
+coord points[MAXN];
+//存储极大点下标的栈
+stack<int> mystack;
 //读取函数并排序
 void input(){
     cin >> n;
@@ -37,6 +43,27 @@ void examine(){
     }
 }
 
+//从前向后扫描寻找极大点
+void find_maxmimal_points(){
+    int temp_max_y=INT32_MIN;
+    for(int i=1;i<=n;i++){
+        if(points[i].second>temp_max_y){
+            mystack.push(i);
+            temp_max_y = points[i].second;
+        }
+    }
+}
+
+//打印函数
+void output(){
+    while(!mystack.empty()){
+        int tmp;
+        tmp = mystack.top();
+        cout<<"("<<points[tmp].first<<","<<points[tmp].second<<")";
+        mystack.pop();
+    }
+}
+
 int main()
 {
     //输入优化
@@ -45,6 +72,7 @@ int main()
     //读取
     input();
     examine();
-
+    find_maxmimal_points();
+    output();
     return 0;
 }
