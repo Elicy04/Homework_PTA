5036961f17cda5379107efc38a1e5f66ccdbf4d3
Elicy04 <Elicy04@outlook.com>
2025-04-17 17:06:23 +0800

    1.2x初始化

---
+++ Algorithm/1.2x/myMain.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void input(){
+
+}
+
+
+void output(){
+
+}
+
+int main(){
+    // 输入优化
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    //输入
+    input();
+    //处理
+
+
+    //输出
+    output();
+    return 0;
+}
