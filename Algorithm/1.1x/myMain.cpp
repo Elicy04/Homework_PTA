1d3082dd79549fabc23712642e7603a99290454a
Elicy04 <Elicy04@outlook.com>
2025-04-17 15:31:59 +0800

    模板更新

--- Algorithm/1.1x/myMain.cpp
+++ Algorithm/1.1x/myMain.cpp
@@ -2,13 +2,25 @@
 
 using namespace std;
 
+void input(){
+
+}
 
 
+void output(){
 
+}
 
 int main(){
+    // 输入优化
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    //输入
+    input();
+    //处理
 
 
-    
+    //输出
+    output();
     return 0;
 }
