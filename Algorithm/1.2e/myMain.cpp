c43b5c81056279748b95f615e4918fe4edc6ff6a
Elicy04 <Elicy04@outlook.com>
2025-04-18 17:45:48 +0800

    1.2ePass

--- Algorithm/1.2e/myMain.cpp
+++ Algorithm/1.2e/myMain.cpp
@@ -2,25 +2,130 @@
 
 using namespace std;
 
-void input(){
+typedef pair<int, int> work;
 
+// 总收益
+long long sum = 0; // 经过mingjia3同学提醒
+
+// 任务数量
+int n = 0;
+
+// 存储任务的动态数组
+vector<work> worklist;
+
+// 比较函数
+bool cmp(const work &a, const work &b)
+{
+    return a.second > b.second;
 }
 
+// 实现并查集
+class UnionFind
+{
+public:
+    vector<int> parent; // 父节点标识符
+    // vector<int> rank;   // 树高度
+public:
+    // 构造函数
+    UnionFind(int size)
+    {
+        parent.resize(size + 1);
+        // rank.resize(size + 1, 0);
+        for (int i = 0; i <= size; i++)
+        {
+            parent[i] = i;
+        }
+    }
+    // // 查找
+    // int find(int x)
+    // {
+    //     if (parent[x] != x)
+    //     {
+    //         parent[x] = find(parent[x]);
+    //     }
+    //     return parent[x];
+    // }
+    // 查找（非递归）
+    int find(int x){
+        while(parent[x]!=x){
+            parent[x] = parent[parent[x]];
+            x = parent[x];
+        }
+        return x;
+    }
+    // // 合并
+    // void unite(int x, int y)
+    // {
+    //     int rootX = find(x);
+    //     int rootY = find(y);
+    //     // 如果在同一集合
+    //     if (rootX == rootY)
+    //         return;
+    //     // 否则合并
+    //     if (rank[rootX] < rank[rootY])
+    //     {
+    //         parent[rootX] = rootY;
+    //     }
+    //     else
+    //     {
+    //         parent[rootY] = rootX;
+    //         if (rank[rootX] == rank[rootY])
+    //         {
+    //             rank[rootX]++;
+    //         }
+    //     }
+    // }
+    // 判断两个元素是否在同一集合
+    bool isConnected(int x, int y)
+    {
+        return find(x) == find(y);
+    }
+};
 
-void output(){
-
+void input()
+{
+    cin >> n;
+    worklist.resize(n + 1);
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> worklist[i].first >> worklist[i].second;
+    }
 }
 
-int main(){
+void get_sum()
+{
+    UnionFind uf(n);
+    sort(worklist.begin() + 1, worklist.end(), cmp);
+    for (int i = 1, j = 0; i <= n; i++)
+    {
+        j = uf.find(min(worklist[i].first,n));
+        if (j != 0)
+        {
+            sum += worklist[i].second;
+            uf.parent[j] = uf.find(j-1);
+        }
+    }
+}
+
+void output()
+{
+    cout << sum <<endl;
+}
+
+int main()
+{
     // 输入优化
     cin.sync_with_stdio(0);
     cin.tie(0);
-    //输入
+    // 输入
     input();
-    //处理
-
-
-    //输出
+    // 处理
+    get_sum();
+    // 输出
     output();
     return 0;
 }
+
+//Q*1:极大问题：对于代码逻辑完全无误，而最后一个样例点始终报段错误的原因
+//A*1:已查明：来源：cmp函数中的比较使用了 ">="而不是">"
+//这道题无排序稳定性要求，此
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Homework_PTA
 some code quesetion and my solution
+本人水平有限，如有错误或更好的方法欢迎issue和PR，愿与君共勉
+p.s:1.1e/x
+    其中第一个数字表示第几次作业，第二个数字表示题目序号，e/x分别表示必做题/选做题
